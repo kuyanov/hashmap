74755c9a628749de2886b1e92932cfca4ef420bb
feodor <feodor.kuyanov@gmail.com>
2021-06-23 18:12:49 +0300

    Removed unnecessary files and updated code style

--- hash_map.h
+++ hash_map.h
@@ -13,6 +13,10 @@
 #define HASHMAP_HASH_MAP_H
 
 // Hash table with separate chaining, dynamic expansion.
+// For each hash there is a linked list with elements of that hash.
+// When the number of elements exceeds the number of buckets, the number of buckets double and hashes are recomputed.
+// All queries work for O(length of chain) which is O(1) in average.
+// Detailed explanation is there: https://en.wikipedia.org/wiki/Hash_table#Separate_chaining
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
   private:
@@ -21,60 +25,34 @@ class HashMap {
     using ElemIter = typename std::list<ConstKeyVal>::iterator;
     using ConstElemIter = typename std::list<ConstKeyVal>::const_iterator;
 
-    Hash hasher;
-    size_t all_size;
-    std::list<ConstKeyVal> all;
-    std::vector<std::list<ElemIter>> table;
-
-    // Bucket number by key.
-    size_t bucket_id(const KeyType &key) const {
-        return hasher(key) % table.size();
-    }
-
-    // Resizes the array of buckets.
-    void resize(size_t table_size) {
-        table.clear();
-        table.resize(table_size);
-        for (ElemIter it = all.begin(); it != all.end(); ++it) {
-            table[bucket_id(it->first)].push_front(it);
-        }
-    }
-
-    // Doubles the number of buckets if the number of elements exceeded it.
-    void resize_if_necessary() {
-        if (all_size > table.size()) {
-            resize(2 * table.size());
-        }
-    }
-
   public:
     explicit HashMap(Hash custom_hasher = Hash()) : hasher(custom_hasher) {
-        all_size = 0;
+        allSize = 0;
         resize(1);
     }
 
     template<class ForwardIterator>
     HashMap(ForwardIterator begin, ForwardIterator end, Hash custom_hasher = Hash())
             : hasher(custom_hasher) {
-        all_size = 0;
+        allSize = 0;
         while (begin != end) {
             all.push_front(*begin);
-            ++all_size;
+            ++allSize;
             ++begin;
         }
-        resize(2 * all_size);
+        resize(2 * allSize);
     }
 
     HashMap(std::initializer_list<KeyVal> elements, Hash custom_hasher = Hash())
             : hasher(custom_hasher) {
         std::copy(elements.begin(), elements.end(), std::back_inserter(all));
-        all_size = elements.size();
-        resize(2 * all_size);
+        allSize = elements.size();
+        resize(2 * allSize);
     }
 
     HashMap(const HashMap &other) {
         hasher = other.hasher;
-        all_size = other.all_size;
+        allSize = other.allSize;
         table.resize(other.table.size());
         for (size_t id = 0; id < other.table.size(); ++id) {
             for (ConstElemIter it : other.table[id]) {
@@ -89,11 +67,11 @@ class HashMap {
         table.clear();
         table.resize(other.table.size());
         all.clear();
-        all_size = 0;
+        allSize = 0;
         for (size_t id = 0; id < other.table.size(); ++id) {
             for (ConstElemIter it : other.table[id]) {
                 all.push_front(*it);
-                ++all_size;
+                ++allSize;
                 table[id].push_front(all.begin());
             }
         }
@@ -102,12 +80,12 @@ class HashMap {
 
     // Actual number of elements in the hash table.
     size_t size() const {
-        return all_size;
+        return allSize;
     }
 
     // Checks if the hash table is empty.
     bool empty() const {
-        return all_size == 0;
+        return allSize == 0;
     }
 
     // Hasher function.
@@ -117,27 +95,27 @@ class HashMap {
 
     // Inserts pair (key, value) into the hash table. If such key exists, nothing is inserted.
     void insert(const KeyVal &p) {
-        size_t id = bucket_id(p.first);
+        size_t id = bucketId(p.first);
         for (ConstElemIter it : table[id]) {
             if (it->first == p.first) {
                 return;
             }
         }
         all.push_front(p);
-        ++all_size;
+        ++allSize;
         table[id].push_front(all.begin());
-        resize_if_necessary();
+        resizeIfNecessary();
     }
 
     // Erases the given key from the hash table. If there is no such key, nothing happens.
     void erase(const KeyType &key) {
-        size_t id = bucket_id(key);
+        size_t id = bucketId(key);
         for (auto iter = table[id].begin(); iter != table[id].end(); ++iter) {
             ConstElemIter it = *iter;
             if (it->first == key) {
                 table[id].erase(iter);
                 all.erase(it);
-                --all_size;
+                --allSize;
                 return;
             }
         }
@@ -147,28 +125,28 @@ class HashMap {
     using const_iterator = ConstElemIter;
 
     // Iterator for begin.
-    iterator begin() {
+    inline iterator begin() {
         return all.begin();
     }
 
     // Const iterator for begin
-    const_iterator begin() const {
+    inline const_iterator begin() const {
         return all.begin();
     }
 
     // Iterator for end
-    iterator end() {
+    inline iterator end() {
         return all.end();
     }
 
     // Const iterator for end
-    const_iterator end() const {
+    inline const_iterator end() const {
         return all.end();
     }
 
     // Returns iterator for the element with the given key. If there is no such key, end() is returned.
     iterator find(const KeyType &key) {
-        size_t id = bucket_id(key);
+        size_t id = bucketId(key);
         for (ElemIter it : table[id]) {
             if (it->first == key) {
                 return it;
@@ -179,7 +157,7 @@ class HashMap {
 
     // Const version of find()
     const_iterator find(const KeyType &key) const {
-        size_t id = bucket_id(key);
+        size_t id = bucketId(key);
         for (ConstElemIter it : table[id]) {
             if (it->first == key) {
                 return it;
@@ -190,26 +168,23 @@ class HashMap {
 
     // Access to value by key. If the key is absent, (key, ValueType()) is inserted.
     ValueType &operator[](const KeyType &key) {
-        size_t id = bucket_id(key);
-        for (ElemIter it : table[id]) {
-            if (it->first == key) {
-                return it->second;
-            }
+        ElemIter it = find(key);
+        if (it != end()) {
+            return it->second;
         }
+        size_t id = bucketId(key);
         all.emplace_front(key, ValueType());
-        ++all_size;
+        ++allSize;
         table[id].push_front(all.begin());
-        resize_if_necessary();
+        resizeIfNecessary();
         return all.begin()->second;
     }
 
     // Read access to value by key. If the key is absent, std::out_of_range is thrown.
     const ValueType &at(const KeyType &key) const {
-        size_t id = bucket_id(key);
-        for (ConstElemIter it : table[id]) {
-            if (it->first == key) {
-                return it->second;
-            }
+        ConstElemIter it = find(key);
+        if (it != end()) {
+            return it->second;
         }
         throw std::out_of_range("");
     }
@@ -217,11 +192,38 @@ class HashMap {
     // Erases all data from the hash table.
     void clear() {
         for (const auto &elem : all) {
-            size_t id = bucket_id(elem.first);
+            size_t id = bucketId(elem.first);
             table[id].clear();
         }
         all.clear();
-        all_size = 0;
+        allSize = 0;
+    }
+
+  private:
+    Hash hasher;
+    size_t allSize;
+    std::list<ConstKeyVal> all;
+    std::vector<std::list<ElemIter>> table;
+
+    // Bucket number by key.
+    size_t bucketId(const KeyType &key) const {
+        return hasher(key) % table.size();
+    }
+
+    // Resizes the array of buckets.
+    void resize(size_t tableSize) {
+        table.clear();
+        table.resize(tableSize);
+        for (ElemIter it = all.begin(); it != all.end(); ++it) {
+            table[bucketId(it->first)].push_front(it);
+        }
+    }
+
+    // Doubles the number of buckets if the number of elements exceeded it.
+    void resizeIfNecessary() {
+        if (allSize > table.size()) {
+            resize(2 * table.size());
+        }
     }
 };
 
